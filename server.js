af912de588ff8fc212b56b43e150c938209c7318
Sinan Sinan <18sos1@queensu.ca>
2020-10-01 08:19:18 -0400

    t

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'development'){
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(`${__dirname}/build`));
-//
+
     app.get(/.*/, (req, res) => res.sendFile(`build/index.html`, { root: __dirname }));
 }
 
